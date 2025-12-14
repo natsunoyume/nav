e993f284bdf3829a6fccb7f3299f06bcf4760c23
natsunoyume <130218209+natsunoyume@users.noreply.github.com>
2025-12-14 02:00:48 +0000

    Deploying to gh-pages from @ natsunoyume/nav@bde7b46dd13041070cbbf72f2b573f21aa8513ba 🚀

---
+++ chunk-72DLSVFX.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-566HWFRW.js";import{qb as r,zb as e}from"./chunk-TFYMS3KA.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
